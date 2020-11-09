07f5e2f644e5612b08138960a78bd0cdea216e38
Puja <pujayadav@gmail.com>
2020-11-10 00:58:04 +0530

    [puja] Refactor. Update salary

--- Employee_Management/Employee_Management/EmployeeRepo.cs
+++ Employee_Management/Employee_Management/EmployeeRepo.cs
@@ -52,5 +52,74 @@ namespace EmployeePayrollService
                 System.Console.WriteLine(e.Message);
             }
         }
+        public bool AddEmployee(EmployeeModel model)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                using (connection)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@BasicPay", model.BasicPay);
+                    command.Parameters.AddWithValue("@Deductions", model.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", model.Tax);
+                    command.Parameters.AddWithValue("@NetPay", model.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", DateTime.Now);
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        public bool UpdateSalary(string name, decimal salary)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                using (connection)
+                {
+                    string query = @"Update employee_payroll set basic_pay = '" + salary + "' where name = '" + name + "'";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
     }
 }
--- Employee_Management/Employee_Management/Program.cs
+++ Employee_Management/Employee_Management/Program.cs
@@ -8,7 +8,47 @@ namespace EmployeePayrollService
         {
             Console.WriteLine("Emplyee Payroll problem!");
             EmployeeRepo repo = new EmployeeRepo();
-            repo.GetAllEmployee();
+            int loop = 1;
+            while (loop == 1)
+            {
+                Console.WriteLine("Choose \n1. View all records \n2. Add record \n3. Update salary \n4. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        repo.GetAllEmployee();
+                        break;
+                    case 2:
+                        EmployeeModel employee = new EmployeeModel();
+                        Console.WriteLine("Enter Name");
+                        employee.EmployeeName = Console.ReadLine();
+                        Console.WriteLine("Enter Department");
+                        employee.Department = Console.ReadLine();
+                        Console.WriteLine("Enter Phone Number");
+                        employee.PhoneNumber = Console.ReadLine();
+                        Console.WriteLine("Enter Address");
+                        employee.Address = Console.ReadLine();
+                        Console.WriteLine("Enter Gender");
+                        employee.Gender = Convert.ToChar(Console.ReadLine());
+                        Console.WriteLine("Enter Basic Pay");
+                        employee.BasicPay = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("Enter Deductions");
+                        employee.Deductions = (double)Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("Enter Start date");
+                        employee.StartDate = Convert.ToDateTime(Console.ReadLine());
+                        if (repo.AddEmployee(employee))
+                            Console.WriteLine("Records added successfully");
+                        break;
+                    case 3:
+                        string name = Console.ReadLine();
+                        decimal salary = Convert.ToDecimal(Console.ReadLine());
+                        repo.UpdateSalary(name, salary);
+                        break;
+                    case 4:
+                        loop = 0;
+                        break;
+                }
+            }
         }
     }
 }
